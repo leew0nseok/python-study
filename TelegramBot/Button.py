155c63195ab0003a5febb4f4828e70282ab54a59
leew0nseok <zzang50aa@gmail.com>
2022-05-09 23:06:55 +0900

    practace

--- TelegramBot/Button.py
+++ TelegramBot/Button.py
@@ -3,14 +3,10 @@ from telepot.loop import MessageLoop
 from telepot.namedtuple import InlineKeyboardMarkup
 from telepot.namedtuple import InlineKeyboardButton
 
-my_token = ""
-bot = telegram.Bot(token="")
-chat_id = 5081938343
+token = ""
+bot = telepot.Bot(token)
 
-btn_key = InlineKeyboardButton(
-    text="1. 학교위치", callback_data="1")
-btn_key2 = InlineKeyboardButton(
-    text="2. 학사일정", callback_data="2")
+chat_id =
 
 # 메세지 처리 함수
 
@@ -21,7 +17,9 @@ def btn_show(msg):
         text="1. 학교위치", callback_data="1")
     btn_key2 = InlineKeyboardButton(
         text="2. 학사일정", callback_data="2")
-    mu = InlineKeyboardMarkup(inline_keyboard=[[btn_key, btn_key2]])
+    btn_key3 = InlineKeyboardButton(
+        text="3. 학사", callback_data="3")
+    mu = InlineKeyboardMarkup(inline_keyboard=[[btn_key, btn_key2, btn_key3]])
     bot.sendMessage(chat_id, "선택하세요", reply_markup=mu)
 
 
@@ -29,14 +27,12 @@ def query_ans(msg):
     query_id = msg["id"]
     query_data = msg["data"]
     if query_data == "1":
-        bot.sendMessage(query_id, text="경기도 용인시 처인구 모현읍 외대로 81 (왕산리)")
+        bot.sendMessage(chat_id, text="경기도 용인시 처인구 모현읍 외대로 81 (왕산리)")
     elif query_data == "2":
-        bot.answerCallbackQuery(query_id, text="학사일정")
+        bot.sendMessage(chat_id, text="학사일정")
 
+
+bot.sendMessage(chat_id, "hihihi")
 
 MessageLoop(
     bot, {'chat': btn_show, 'callback_query': query_ans}).run_as_thread()
-
-
-'''bot.sendMessage(chat_id=chat_id,
-                text="경기도 용인시 처인구 모현읍 외대로 81 (왕산리)")  # 메세지 보내기'''
--- TelegramBot/telegramBot.py
+++ TelegramBot/telegramBot.py
@@ -1,84 +1,53 @@
+
+# 모듈설정
 import telegram
-from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
-from telegram.ext import MessageHandler, Filters
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+import telepot  # pip install telepot
+import psutil
+import os
+from telepot.loop import MessageLoop  # 봇구동
+from telepot.namedtuple import InlineKeyboardMarkup as MU  # 마크업
+from telepot.namedtuple import InlineKeyboardButton as BT  # 버튼
+import time
+import sys
 
-# token = "5382062842:AAFy953rQtlu2l6M0DIpzQCdJXJrWAJemU4"
-# bot = telegram.Bot(token="5382062842:AAFy953rQtlu2l6M0DIpzQCdJXJrWAJemU4")
-
-chat_id = 5081938343
-
-
-with open("token.txt") as f:
-    lines = f.readlines()
-    token = lines[0].strip()
-
-# updater
-updater = Updater(token=token, use_context=True)
-dispatcher = updater.dispatcher
+# 알림받을 텔레그램 사용자 정보 입력
+token = "5382062842:AAFy953rQtlu2l6M0DIpzQCdJXJrWAJemU4"
+mc = '5081938343'
+bot = telepot.Bot(token)
 
 
-# command hander
-def start(update, context):
-    context.bot.send_message(
-        chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
-
-
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
-
-# polling
-updater.start_polling()
-
-bot.sendMessage(chat_id=chat_id,
-                text="경기도 용인시 처인구 모현읍 외대로 81 (왕산리)")  # 메세지 보내기
-
-
-'''def build_menu(buttons, n_cols, header_buttons=None, footer_buttons=None):
-    menu = [buttons[i:i + n_cols] for i in range(0, len(buttons), n_cols)]
-    if header_buttons:
-        menu.insert(0, header_buttons)
-    if footer_buttons:
-        menu.append(footer_buttons)
-    return menu
-
-
-def get_command(update, context):
-    print("get")
-    show_list = []
-    show_list.append(InlineKeyboardButton(
-        "학교위치", callback_data="경기도 용인시 처인구 모현읍 외대로 81 (왕산리)"))  # add on button
-    show_list.append(InlineKeyboardButton(
-        "학사정보", callback_data="학사정보"))  # add off button
-    show_list.append(InlineKeyboardButton(
-        "cancel", callback_data="cancel"))  # add cancel button
-    show_markup = InlineKeyboardMarkup(build_menu(
-        show_list, len(show_list) - 1))  # make markup
-
-    update.message.reply_text("원하는 값을 선택하세요", reply_markup=show_markup)
-
-
-btn_key = InlineKeyboardButton(
-    text="1. 학교위치", callback_data="1")
-btn_key2 = InlineKeyboardButton(
-    text="2. 학사일정", callback_data="2")
-
+# IP 버튼 생성
 
 def btn_show(msg):
-    input_chat = msg['text']
-    btn_key = InlineKeyboardButton(
-        text="1. 학교위치", callback_data="1")
-    btn_key2 = InlineKeyboardButton(
-        text="2. 학사일정", callback_data="2")
-    mu = InlineKeyboardMarkup(inline_keyboard=[[btn_key, btn_key2]])
-    bot.sendMessage(chat_id, "선택하세요", reply_markup=mu)
+    btn1 = BT(text="1.1.1.1", callback_data="1")
+    btn2 = BT(text="1.1.1.1", callback_data="2")
+
+    # IP 버튼 가로 정렬
+    mu = MU(inline_keyboard=[[btn1, btn2]])
+
+    # 1차 응답 (버튼제목)
+    bot.sendMessage(mc, " ☞  모니터링할 IP를 선택하세요 ", reply_markup=mu)
+
+# 콜백 받은 대답 처리
 
 
 def query_ans(msg):
-    query_id = msg["id"]
     query_data = msg["data"]
+    f = open('../data.txt')
     if query_data == "1":
-        bot.sendMessage(query_id, text="경기도 용인시 처인구 모현읍 외대로 81 (왕산리)")
+        lines = f.readline()
+        bot.sendMessage(mc, "▷ Memory Usage : "+lines.split(',')[1]+"\n" + "▷ Disk Usage : "+lines.split(
+            ',')[2]+"\n" + "▷ Cpu Load : "+lines.split(',')[3].rstrip('\n'))
     elif query_data == "2":
-        bot.answerCallbackQuery(query_id, text="학사일정")
-'''
+        lines = f.readline()
+        lines = f.readline()
+        bot.sendMessage(mc, "▷ Memory Usage : "+lines.split(',')[1]+"\n" + "▷ Disk Usage : "+lines.split(
+            ',')[2]+"\n" + "▷ Cpu Load : "+lines.split(',')[3].rstrip('\n'))
+
+
+MessageLoop(
+    bot, {'chat': btn_show, 'callback_query': query_ans}). run_as_thread()
+
+# 코드가 참일 경우 프로세스 계속 유지
+while True:
+    time.sleep(5)
