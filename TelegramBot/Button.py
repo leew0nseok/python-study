4ac872aea81776433f744ffd2e13d90055745d22
leew0nseok <zzang50aa@gmail.com>
2022-05-10 21:42:42 +0900

    GUI

--- TelegramBot/Button.py
+++ TelegramBot/Button.py
@@ -18,10 +18,13 @@ def btn_show(msg):
     btn_key = InlineKeyboardButton(
         text="1. 학교위치", callback_data="1")
     btn_key2 = InlineKeyboardButton(
-        text="2. 학사일정", callback_data="2")
+        text="2. 학사", callback_data="2")
     btn_key3 = InlineKeyboardButton(
-        text="3. 학사", callback_data="3")
-    mu = InlineKeyboardMarkup(inline_keyboard=[[btn_key, btn_key2, btn_key3]])
+        text="3. 수업", callback_data="3")
+    btn_key4 = InlineKeyboardButton(
+        text="4. 전공", callback_data="4")
+    mu = InlineKeyboardMarkup(
+        inline_keyboard=[[btn_key, btn_key2, btn_key3], [btn_key4]])
     bot.sendMessage(chat_id, "선택하세요", reply_markup=mu)
 
 
@@ -29,12 +32,38 @@ def query_ans(msg):
     query_id = msg["id"]
     query_data = msg["data"]
     if query_data == "1":
+        bot.sendMessage(chat_id, text="학교위치를 선택하셨습니다.")
         bot.sendMessage(chat_id, text="경기도 용인시 처인구 모현읍 외대로 81 (왕산리)")
     elif query_data == "2":
+        btn_key = InlineKeyboardButton(
+            text="1. 학사일정", callback_data="학사일정")
+        btn_key2 = InlineKeyboardButton(
+            text="2. 학사안내", callback_data="학사안내")
+        mu = InlineKeyboardMarkup(
+            inline_keyboard=[[btn_key, btn_key2]])
+        bot.sendMessage(chat_id, "학사를 선택하셨습니다.", reply_markup=mu)
+    elif query_data == "3":
+        btn_key = InlineKeyboardButton(
+            text="1. 수강신청", callback_data="수강신청")
+        btn_key2 = InlineKeyboardButton(
+            text="2. 계절학기", callback_data="계절학기")
+        btn_key3 = InlineKeyboardButton(
+            text="3. 성적", callback_data="성적")
+        mu = InlineKeyboardMarkup(
+            inline_keyboard=[[btn_key, btn_key2, btn_key3]])
+        bot.sendMessage(chat_id, "수업을 선택하셨습니다.", reply_markup=mu)
+    elif query_data == "학사일정":
+        bot.sendMessage(chat_id, text="학사일정을 선택하셨습니다.")
         f = open('C:\\Users\\LEE\\Desktop\\StudyPython\\Datas\\학사일정.PNG', 'rb')
         bot.sendPhoto(chat_id, f)
-    elif query_data == "3":
-        bot.sendMessage(chat_id, text="^^")
+    elif query_data == "수강신청":
+        bot.sendMessage(chat_id, text="수강신청을 선택하셨습니다.")
+    elif query_data == "계절학기":
+        bot.sendMessage(chat_id, text="계절학기를 선택하셨습니다.")
+    elif query_data == "성적":
+        bot.sendMessage(chat_id, text="성적을 선택하셨습니다.")
+        f = open('C:\\Users\\LEE\\Desktop\\StudyPython\\Datas\\성적.PNG', 'rb')
+        bot.sendPhoto(chat_id, f)
 
 
 bot.sendMessage(chat_id, "hihihi")
