a92a1ae7ae0229fa694586df40ee43a436e87a4f
leew0nseok <zzang50aa@gmail.com>
2022-05-24 16:03:13 +0900

    Design DPS code

---
+++ Graph/DFS.py
@@ -0,0 +1,12 @@
+'''
+def DPS(S):
+  stack.push((None, S)):
+  while stack is not empty:
+    p, v = stack.pop()
+    if v is unmarked:
+      mark[v] = "unvisited"
+      parent[v] = p
+      for each edge(v,w):
+        if w is unmarked:
+          stack.push((v,w)
+'''
