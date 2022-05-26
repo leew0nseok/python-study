1aa2b9ae211b0d19769afd1dfbbaa525a68b4577
leew0nseok <zzang50aa@gmail.com>
2022-05-26 12:39:46 +0900

    Dot product

--- Loops - Dot product/Dot_product.1650081734392/Main.py
+++ Loops - Dot product/Dot_product.1650081734392/Main.py
@@ -1,11 +1,13 @@
-
+# Dot product
 # write the dot_product function here
 def dot_product(a, b):
-	dot = 0
-	for i in range(len(a)):
-		dot += a[i] * b[i]
-	return dot
+    dot = 0
+    for i in range(len(a)):
+        dot += a[i] * b[i]
+    return dot
+
+
 a = [int(x) for x in input().split()]
 b = [int(x) for x in input().split()]
-dot = dot_product(a,b)
+dot = dot_product(a, b)
 print(dot)
