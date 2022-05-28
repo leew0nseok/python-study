071056a82b6afd0770529c9fcb8b35288933fc8d
leew0nseok <zzang50aa@gmail.com>
2022-05-28 18:17:00 +0900

    testpdfminer

--- Crawlling/frompdfminer.py
+++ Crawlling/frompdfminer.py
@@ -61,6 +61,30 @@ def parse_obj(lt_objs, str):
             parse_obj(obj._objs)
 
 
+def parse_obj2(lt_objs, n):
+    title = []
+    # loop over the object list
+    for obj in lt_objs:
+
+        # if it's a textbox, print text and location
+        if isinstance(obj, pdfminer.layout.LTTextBoxHorizontal):
+            if obj.bbox[0] <= n and not obj.get_text().startswith("  "):
+                '''
+                print ("%6d %6d %6d %6d %s" % (obj.bbox[0], obj.bbox[3],
+                                           obj.bbox[2], obj.bbox[1],
+                                           obj.get_text().replace('\n', '_')))
+                                           '''
+                title.append(obj.bbox[3])  # y 값 리스트에 넣기
+                print(obj.get_text())
+                print(obj.bbox[0])
+            else:
+                continue
+        # if it's a container, recurse
+        elif isinstance(obj, pdfminer.layout.LTFigure):
+            parse_obj(obj._objs)
+    return title
+
+
 # loop over all pages in the document
 pg = input()  # pagenumber
 pgint = int(pg)  # 찾을 문자열
@@ -74,6 +98,7 @@ for page in PDFPage.create_pages(document):
         layout = device.get_result()
 
     # extract text from this object
+        title = parse_obj2(layout._objs, 56.7)
         a, b, c, d = parse_obj(layout._objs, str)
         # print(a,b,c,d)
 
@@ -81,7 +106,16 @@ for page in PDFPage.create_pages(document):
     # image size = 590x834
         img = img.resize((590, 834))
     # x start, y start, x length, y length
-        crop_area = (a, 834-b, 590, 834)
+        if len(title) > 0:
+            for i in title:
+                if d >= i:
+                    d = i
+                    crop_area = (a, 834-b, 590, 834-d)
+                    break
+                else:
+                    crop_area = (a, 834-b, 590, 770)
+        else:
+            crop_area = (0, 834-b, 590, 770)
 
         cropped_img = img.crop(crop_area)
         cropped_img.show()
