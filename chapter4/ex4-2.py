2bfd47c635f56af627243b3d1564a22f8f1c240d
leew0nseok <zzang50aa@gmail.com>
2022-09-29 11:42:30 +0900

    chapter4

---
+++ chapter4/ex4-2.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+cnt = 0
+
+for hour in range(n+1):
+  for min in range(60):
+    for sec in range(60):
+      # sec += j
+      if "3" in str(hour)+ str(min) + str(sec):
+        cnt += 1
+
+print(cnt)
